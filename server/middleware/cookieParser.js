bdb6218783f20d2d13437902cfc1a3e2a353d255
Ziqian Li <liziqian1992@gmail.com>
2022-05-10 20:06:14 -0700

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
 const parseCookies = (req, res, next) => {
+  var cookies = req.headers.cookie;
+  if (cookies) {
+    var cookiesArr = cookies.split('; ');
+    cookiesArr.forEach((cookie) => {
+      var keyValue = cookie.split('=');
+      var key = keyValue[0];
+      var value = keyValue[1];
+      req.cookies[key] = value;
+    });
+  } else {
+    req.cookies = {};
+  }
+  next();
 };
 
 module.exports = parseCookies;
