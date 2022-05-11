8fac93ff51d070b56a7b2245b477bb2a3a41bb62
Ziqian Li <liziqian1992@gmail.com>
2022-05-10 21:10:46 -0700

    make change of createSession in auto.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,25 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  var cookie = req.cookies;
+  console.log(cookie);
+  if (!Object.keys(cookie).length) {
+    models.Sessions.create()
+      .then(function (hash) {
+        return models.Sessions.get({id: hash.insertId});
+      })
+      .then(function (result) {
+        console.log(result);
+        res.cookie('shortlyid', result.hash);
+        req.session = {};
+        console.log(req.session);
+        req.session.hash = result.hash;
+        next();
+      })
+  } else if (cookie) {
+
+  }
+
 };
 
 /************************************************************/
